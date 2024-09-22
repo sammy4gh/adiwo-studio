611e0b45b198a702c1e49774a88eead0cb3f48a1
sammy <aristocrat4gh@gmail.com>
2024-09-22 12:32:32 +0000

    set calendar view

--- src/app/studio/_components/event-calender.tsx
+++ src/app/studio/_components/event-calender.tsx
@@ -1,18 +1,35 @@
 "use client";
-import React, { Fragment } from "react";
-import { Calendar, luxonLocalizer, Views } from "react-big-calendar";
+import React, { Fragment, useMemo, useState } from "react";
+import { Calendar, luxonLocalizer, View, Views } from "react-big-calendar";
 import { DateTime } from "luxon";
+import { useSearchParams } from "next/navigation";
+import { useAppSearchParams } from "@/app/hooks/use-app-search-params";
 
 export default function EventCalender() {
   const localizer = luxonLocalizer(DateTime);
-
-  const [view, setView] = React.useState(Views.WEEK);
+  const defaultDate = DateTime.local().toJSDate();
+  const { setSearchParam, searchParams } = useAppSearchParams();
+  const onView = (view: View) => {
+    setSearchParam({ view: view });
+  };
 
   return (
-    <Fragment>
-      <div className="h-screen">
-        <Calendar localizer={localizer} defaultView={Views.WEEK} />
-      </div>
-    </Fragment>
+    <div className="h-screen">
+      <Calendar
+        key={searchParams.toString()}
+        localizer={localizer}
+        defaultDate={defaultDate}
+        defaultView={searchParams.get("view") as View}
+        views={[
+          Views.MONTH,
+          Views.WEEK,
+          Views.WORK_WEEK,
+          Views.DAY,
+          Views.AGENDA,
+        ]}
+        step={60}
+        onView={onView}
+      />
+    </div>
   );
 }
